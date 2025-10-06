fd4ce8dea966f9667162b9d33d5daceca3a7ccbf
leledydy <lelediway@proton.me>
2025-10-06 09:28:59 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+import express from 'express';
+const app = express();
+app.get('/', (_req, res) => res.send('OK'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Health server on :${port}`));
