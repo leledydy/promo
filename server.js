cbb890e24bc6cba394e88fee63448be721fadf9a
leledydy <lelediway@proton.me>
2025-10-06 09:01:32 +0700

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-const app = express();
-
-app.get('/', (_req, res) => res.send('OK'));
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`🌐 Keep-alive server on :${port}`));
